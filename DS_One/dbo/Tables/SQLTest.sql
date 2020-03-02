5924f863bb5a553767e4b50b5e262e5904f0a90c
kravi2020 <61031719+kravi2020@users.noreply.github.com>
2020-03-02 14:36:27 -0600

    Added New Column

--- DS_One/dbo/Tables/SQLTest.sql
+++ DS_One/dbo/Tables/SQLTest.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[SQLTest] (
     [id] INT NULL,
     [Name] varchar(20) null,
-    [ProcName] varchar(20) Null
+    [ProcName] varchar(20) Null,
+    [ProcNames] varchar(10) null
 );
 
